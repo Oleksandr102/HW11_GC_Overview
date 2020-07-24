a91447f789f4026c32ef82c4c0d5fd78085a0bc7
Oleksandr102 <65391094+Oleksandr102@users.noreply.github.com>
2020-07-24 14:53:18 +0300

    Initial commit

---
+++ src/main/java/com/company/execute/Execute.java
@@ -0,0 +1,11 @@
+package main.java.com.company.execute;
+
+public class Execute {
+
+    public void run() {
+        int[] arr = new int[Integer.MAX_VALUE];
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = i + 1;
+    }
+}
